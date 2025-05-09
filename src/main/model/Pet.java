8a3618d8d4ed63a883d34d4ca7a97bdb9f4ffbbf
San Sit <sansit1@students.cs.ubc.ca>
2025-05-08 23:48:57 -0700

    Added specification for Pet abstract class

---
+++ src/main/model/Pet.java
@@ -0,0 +1,94 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// An abatract class representing a pet with an age, name, ability to be hungry, fed, clean, sick, etc..
+public abstract class Pet {
+    private String name;
+    private int age;
+    private boolean isHungry;
+    private boolean isFed;
+    private boolean needsBath;
+    private boolean isSick;
+    private Set<String> edibleFoods;
+
+    // EFFECTS: constructor for pet
+    public Pet(String name) {
+        this.name = name;
+        this.age = 0;
+        this.isHungry = false;
+        this.isFed = true;
+        this.needsBath = false;
+        this.isSick = false;
+        this.edibleFoods = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: grows up by age of 1
+    public void growUp() {
+        this.age++;
+    }
+
+    public boolean isHungry() {
+        return isHungry;
+    }
+
+    public boolean isFed() {
+        return isFed;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: feeds pet; sets isFed to true and isHungry to false
+    public void feed() {
+        this.isFed = true;
+        this.isHungry = false;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: feeds pet; sets isFed to false and isHungry to true
+    public void getsHungry() {
+
+    }
+
+    public boolean needsBath() {
+        return needsBath;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: pet gets dirty and wants bath
+    public void becomesDirty() {
+
+    }
+
+    public boolean isSick() {
+        return isSick;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: pet gets sick
+    public void becomesSick() {
+        
+        
+    }
+    public Set<String> getEdibleFoods() {
+        return edibleFoods;
+    }
+
+    
+
+    
+
+}
